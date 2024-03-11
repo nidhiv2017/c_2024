abc6c0f8174b48d5fecd4c6c1d1d485a21bf589d
nidhiv2017 <116021984+nidhiv2017@users.noreply.github.com>
2024-03-11 11:11:19 +0000

    this program predict the output

---
+++ predict_the_output.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void){
+    int p, q;
+    printf("Enter the value of p and q: ");
+    scanf("%d %d", &p, &q);
+    printf("Value of p: ", p);
+    printf("Value of q: ", q);
+
+    return 0;
+}
